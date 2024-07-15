7ca27e7ab0ded47137e3b64269156218c60e65ee
dsolerh <dsolerh.cinter95@gmail.com>
2024-07-15 16:44:13 +0300

    Add Float and FloatOr implementation

--- numbers.go
+++ numbers.go
@@ -52,10 +52,40 @@ type FloatLike interface {
 	~float32 | ~float64
 }
 
+func FloatSafeConvert[FSrc, FDst FloatLike](val FSrc) (FDst, error) {
+	val2 := FDst(val)
+	if FSrc(val2) != val {
+		return FDst(0), newInvalidConversionError(val, val2)
+	}
+	return val2, nil
+}
+
+func toFloat[F FloatLike](someval any, prop string) (F, error) {
+	switch val := someval.(type) {
+	case F:
+		return val, nil
+	case float32:
+		return FloatSafeConvert[float32, F](val)
+	case float64:
+		return FloatSafeConvert[float64, F](val)
+	default:
+		var zero F
+		return zero, newPropNotOfTypeError(prop, zero)
+	}
+}
+
 func Float[F FloatLike](dict Dict, prop string) (F, error) {
-	return zero[F](), nil
+	if someval, exist := dict[prop]; !exist {
+		return 0, newPropNotPresentError(prop)
+	} else {
+		return toFloat[F](someval, prop)
+	}
 }
 
 func FloatOr[F FloatLike](dict Dict, prop string, defaultVal F) (F, error) {
-	return defaultVal, nil
+	if someval, exist := dict[prop]; !exist {
+		return defaultVal, nil
+	} else {
+		return toFloat[F](someval, prop)
+	}
 }
--- numbers_test.go
+++ numbers_test.go
@@ -83,3 +83,79 @@ func Test_IntegerOr(t *testing.T) {
 		},
 	})
 }
+
+func floatExecutor[T FloatLike](d Dict, prop string) (any, error) {
+	val, err := Float[T](d, prop)
+	return any(val), err
+}
+
+func Test_Float(t *testing.T) {
+	dict := Dict{
+		"f32":     float32(32.0),
+		"f64":     float64(64.0),
+		"int":     123,
+		"f64_max": math.MaxFloat64,
+	}
+	testutils.RunTestCases(t, dict, []testutils.TestCase{
+		{
+			Desc:        "should return a float64 value",
+			Prop:        "f32",
+			Executor:    floatExecutor[float64],
+			ExpectedVal: float64(32.0),
+		},
+		{
+			Desc:        "should return a float32 value",
+			Prop:        "f64",
+			Executor:    floatExecutor[float32],
+			ExpectedVal: float32(64.0),
+		},
+		{
+			Desc:           "should return an error: prop is not present",
+			Prop:           "no-prop",
+			Executor:       floatExecutor[float32],
+			ExpectErr:      true,
+			CheckErrorWith: testutils.IsErrorOfType[*PropNotPresentError],
+		},
+		{
+			Desc:           "should return an error: prop of invalid type",
+			Prop:           "int",
+			Executor:       floatExecutor[float32],
+			ExpectErr:      true,
+			CheckErrorWith: testutils.IsErrorOfType[*PropNotOfTypeError],
+		},
+		{
+			Desc:           "should return an error: invalid downcast",
+			Prop:           "f64_max",
+			Executor:       floatExecutor[float32],
+			ExpectErr:      true,
+			CheckErrorWith: testutils.IsErrorOfType[*InvalidConversionError],
+		},
+	})
+}
+
+func floatOrExecutor[T FloatLike](defaultVal T) testutils.FuncExecutor {
+	return func(m map[string]any, s string) (any, error) {
+		val, err := FloatOr(m, s, defaultVal)
+		return any(val), err
+	}
+}
+
+func Test_FloatOr(t *testing.T) {
+	dict := Dict{
+		"f32": float32(32.0),
+	}
+	testutils.RunTestCases(t, dict, []testutils.TestCase{
+		{
+			Desc:        "should return the value",
+			Prop:        "f32",
+			Executor:    floatOrExecutor(0.0),
+			ExpectedVal: 32.0,
+		},
+		{
+			Desc:        "should return the default value",
+			Prop:        "no-prop",
+			Executor:    floatOrExecutor(float64(64.0)),
+			ExpectedVal: float64(64.0),
+		},
+	})
+}
